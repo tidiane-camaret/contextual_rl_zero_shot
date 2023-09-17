6ec48bf9022273f525d11b64c2f5b86a222141d5
Tidiane <camaret.tidiane@gmai.com>
2023-09-18 00:37:44 +0200

    cluster submission file for implicit_dqn

--- scripts/experiments/carl/carl_dqn.bash
+++ scripts/experiments/carl/carl_dqn.bash
@@ -1,8 +1,7 @@
-for seed in 1 2 3 4
+for seed in 5 6 7 8 9 10
 do
-    : '
-    python3 scripts/experiments/carl/cleanrl_dqn.py --total-timesteps 250000 --track --context_name length --seed $seed --hide_context
-    python3 scripts/experiments/carl/cleanrl_dqn.py --total-timesteps 250000 --track --context_name length --seed $seed 
-    '
-    python3 scripts/experiments/carl/cleanrl_dqn_iida.py --total-timesteps 250000 --track --context_name length --seed $seed 
+
+    python3 scripts/experiments/carl/cleanrl_dqn.py --total-timesteps 5000000 --track --context_name length --seed $seed --hide_context
+    python3 scripts/experiments/carl/cleanrl_dqn.py --total-timesteps 5000000 --track --context_name length --seed $seed 
+    python3 scripts/experiments/carl/cleanrl_dqn_iida.py --total-timesteps 5000000 --track --context_name length --seed $seed 
 done
