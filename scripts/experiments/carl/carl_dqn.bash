72dabe9475abec5fbb6a9326c8695d3a52d8c267
Tidiane <camaret.tidiane@gmai.com>
2023-09-18 17:02:11 +0200

    correct mistake on arguments (must be explicit or hidden)

--- scripts/experiments/carl/carl_dqn.bash
+++ scripts/experiments/carl/carl_dqn.bash
@@ -1,4 +1,4 @@
-for seed in 10 11 12 13 14 
+for seed in 11 12 13 14 
 do
 
     python3 scripts/experiments/carl/cleanrl_dqn.py --total-timesteps 500000 --track --context_name length --seed $seed --context_state explicit
--- scripts/experiments/carl/cleanrl_dqn.py
+++ scripts/experiments/carl/cleanrl_dqn.py
@@ -27,7 +27,7 @@ from carl_wrapper import context_wrapper
 def parse_args():
     # fmt: off
     parser = argparse.ArgumentParser()
-    parser.add_argument("--context_state", type=str, default="implicit",
+    parser.add_argument("--context_state", type=str, default="hidden",
         help="the state of the context feature")
     parser.add_argument("--context_name", type=str, default="gravity",
         help="the name of the context feature")
@@ -162,12 +162,12 @@ poetry run pip install "stable_baselines3==2.0.0a1"
     print("context", args.context_state)
     context_name = args.context_name 
 
-    if args.context_state == "implicit":
+    if args.context_state == "hidden":
         conact_context = False
     elif args.context_state == "explicit":
         conact_context = True
     else:
-        raise ValueError("context_state must be either implicit or explicit")
+        raise ValueError("context_state must be either hidden or explicit")
 
 
     CARLEnv = context_wrapper(CARLEnv, 
