28500219c829fac931d9481965d1c00680b2f728
tidiane-camaret <camaret.tidiane@gmail.com>
2024-03-17 18:30:24 +0100

    remove exp files

---
+++ scripts/run_test.py
@@ -0,0 +1,23 @@
+import importlib
+import os
+import pprint
+import hydra
+
+from meta_rl.algorithms.sac.sac import train_sac
+from meta_rl.algorithms.sac.sac_utils import Args
+
+
+@hydra.main(version_base=None, config_path="../configs/", config_name="base_exp")
+def main(config):
+    pprint.pprint(config)
+    print("Working directory : {}".format(os.getcwd()))
+    
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        main()  # data processing might error out due to multiple jobs doing the same thing
+        print(e)
+
